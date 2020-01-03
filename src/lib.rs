01f5dbe84786fd5eb915ded573bd3bd6104cd27d
Dimitris Zervas <dzervas@dzervas.gr>
2020-01-03 02:59:34 +0200

    socket.h compatibility

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ pub mod connection;
 pub mod channel;
 
 extern crate lazy_static;
+extern crate libc;
 
 use std::net::TcpStream;
 use std::io::{Read, Write};
@@ -21,9 +22,9 @@ use connection::Connection;
 use std::slice::{from_raw_parts, from_raw_parts_mut};
 use lazy_static::lazy_static;
 use std::sync::Mutex;
+use libc::ssize_t;
 
 lazy_static!{
-//    static STREAM: Mut<TcpStream> = Rc::new(RefCell::new());
     static ref CONN: Mutex<Connection> = {
         let tcpstream = TcpStream::connect("127.0.0.1:4444").unwrap();
         let conn = Connection::new(0, Box::new(tcpstream), false, &[1; 32], &[252, 59, 51, 147, 103, 165, 34, 93, 83, 169, 45, 56, 3, 35, 175, 208, 53, 215, 129, 123, 109, 27, 228, 125, 148, 111, 107, 9, 169, 203, 220, 6]).unwrap();
@@ -34,27 +35,22 @@ lazy_static!{
 
 // TODO: bind, listen, accept
 #[no_mangle]
-pub unsafe extern fn connect(_socket: c_int, _sockaddr: *const c_void, _address_len: c_void) -> c_int {
-//    let stream: TcpStream = TcpStream::connect("127.0.0.1:4444").unwrap();
-//    let c = Connection::new(0, &mut stream.try_clone().unwrap(), &mut stream.try_clone().unwrap(), false, &[1; 32], &[252, 59, 51, 147, 103, 165, 34, 93, 83, 169, 45, 56, 3, 35, 175, 208, 53, 215, 129, 123, 109, 27, 228, 125, 148, 111, 107, 9, 169, 203, 220, 6]).unwrap();
-//    let c = Connection::new_box(0, Box::new(stream.try_clone().unwrap()), Box::new(stream.try_clone().unwrap()), false, &[1; 32], &[252, 59, 51, 147, 103, 165, 34, 93, 83, 169, 45, 56, 3, 35, 175, 208, 53, 215, 129, 123, 109, 27, 228, 125, 148, 111, 107, 9, 169, 203, 220, 6]).unwrap();
-
-//    CONN.with(|conn| Rc::into_raw(*conn) as *mut c_void)
+pub extern fn connect(_socket: c_int, _sockaddr: *const c_void, _address_len: *mut c_void) -> c_int {
+    // TODO: Instanciate the connection here? how?
     0
 }
 
 #[no_mangle]
-pub unsafe extern fn send(_socket: c_int, msg: *const c_void, size: usize, _flags: c_int) -> usize {
+pub unsafe extern fn send(_socket: c_int, msg: *const c_void, size: usize, _flags: c_int) -> ssize_t {
     let buf = from_raw_parts(msg as *const u8, size);
-    CONN.lock().unwrap().write(buf).unwrap()
-//    0
+    CONN.lock().unwrap().write(buf).unwrap() as isize
 }
 
 #[no_mangle]
-pub unsafe extern fn recv(_socket: c_int, msg: *mut c_void, size: usize, _flags: c_int) -> usize {
+pub unsafe extern fn recv(_socket: c_int, msg: *mut c_void, size: usize, _flags: c_int) -> ssize_t {
     let buf = from_raw_parts_mut(msg as *mut u8, size);
-    CONN.lock().unwrap().read(buf).unwrap()
-//    0
+    CONN.lock().unwrap().read(buf).unwrap() as isize
 }
+
 //#[no_mangle]
-//pub unsafe extern "C" fn flush(s: &mut connection::Connection) { conn.unwrap().flush().unwrap() }
+//pub unsafe extern fn flush(s: &mut connection::Connection) { CONN.lock().unwrap().flush().unwrap() }
