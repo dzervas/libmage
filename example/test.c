24c013c5a675b38767414f70798896de902a2be4
Dimitris Zervas <dzervas@dzervas.gr>
2020-01-03 03:16:25 +0200

    Remove autogenerated header in examples

--- example/test.c
+++ example/test.c
@@ -4,9 +4,12 @@ int main() {
 	void *addr = calloc(1, 0);
 	void *len = calloc(1, 0);
 
+	printf("connect()\n");
 	connect(0, addr, len);
 
+	printf("send(hello)\n");
 	send(0, "hello", 6, 0);
 
+	printf("bye");
 	return 0;
 }
