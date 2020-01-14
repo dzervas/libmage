78e762ddc4e1d1b78b4647cdda56b492e9ca7b05
Dimitris Zervas <dzervas@dzervas.gr>
2020-01-14 21:13:33 +0200

    Fix coverage.yml errors

--- build.rs
+++ build.rs
@@ -7,10 +7,14 @@ fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    cbindgen::Builder::new()
+    let cgen = cbindgen::Builder::new()
         .with_crate(crate_dir)
         .with_language(C)
-        .generate()
-        .expect("Unable to generate bindings")
-        .write_to_file(format!("{}/../../../mage.h", out_dir));
+        .generate();
+
+    if cgen.is_ok() {
+        cgen.unwrap().write_to_file(format!("{}/../../../mage.h", out_dir));
+    } else {
+        eprintln!("Unable to generate cbindgen headers!");
+    }
 }
